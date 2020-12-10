2a51d2444ff5afcecc9a3730add69bd9ff12d9cb
Dylan McClain <dmcclain@paydici.com>
2020-12-09 21:47:25 -0800

    a new group of contenders enters the arena

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ league = League.create(name: 'cereal')
 
 cereals = %w[
   capn_crunch cinnamon_toast_crunch french_toast_crunch fruit_loops
-  lucky_charms frosted_flakes honey_comb golden_grahams
+  lucky_charms frosted_flakes honey_comb golden_grahams trix life
+  raisin_bran pops kix honey_nut_cheerios frosted_mini_wheats
+  count_chocula cocoa_puffs apple_jacks
 ]
 
 cereals.each do |cereal|
